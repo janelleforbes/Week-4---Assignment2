571add3399d360bff9a1adc0ed022503922ae21d
janelleforbes <janelle.forbes@gmail.com>
2022-10-21 14:10:18 -0400

    revisiosnt o controller nand router

--- app/controllers/contact.controller.server.js
+++ app/controllers/contact.controller.server.js
@@ -6,6 +6,112 @@ export function DisplayLoginPage(req,res,next){
             console.error(err);
             res.end(err);
         }
-        res.render('index',{title: 'contact/login', contact: userCollection});
+        res.render('index',{title: 'Contact Login', contact: userCollection});
+    })
+}
+
+export function ProcessLoginPage(req, res, next){
+    let id = req.params.id;
+    
+    let newContact = contactModel({
+        _id: req.body.id,
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email
+    });
+
+    contactModel.updateOne({_id: id }, newContact, (err, contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+            res.redirect('/contact-login')
+        };
+
+        res.redirect('/contact-list')
+    } )
+}
+
+
+export function DisplayBusinessContactsPage(req, res, next){
+    contactModel.find(function(err, userCollection) {
+
+        //error checking inside connection
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+//render header and footer
+        res.render('index', {title: 'Contact Business List', page:'contact/list' , contact: userCollection});
+    })
+}
+
+//Business Contact Page
+export function ProcessBusinessContactsPage(req, res, next){
+
+    let id = req.params.id;
+    
+    let newContact = contactModel({
+        _id: req.body.id,
+        Name: req.body.Name,
+        Number: req.body.Number,
+        Email: req.body.Email
+    });
+
+    contactModel.updateOne({_id: id }, newContact, (err, contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+    } )
+}
+
+//Contact Update Page
+
+export function DisplayContactUpdatePage(req, res, next){
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Update Contact', page: 'contact/update', contact: contact });
+    });    
+}
+
+export function ProcessContactUpdatePage(req, res, next){
+
+    let id = req.params.id;
+    
+    let newContact = contactModel({
+        _id: req.body.id,
+        Name: req.body.Name,
+        Number: req.body.Number,
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email
+    });
+
+    contactModel.updateOne({_id: id }, newContact, (err, contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/contact-list')
+    } )
+}
+
+export function ProcessContactDelete(req, res, next){
+    let id = req.params.id;
+
+    contactModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/contact-list');
     })
 }
--- app/routes/contact.route.server.js
+++ app/routes/contact.route.server.js
@@ -2,11 +2,25 @@
 import { Router } from "express";
 
 import {  DisplayLoginPage,
+    ProcessLoginPage,
+    DisplayBusinessContactsPage,
+    ProcessBusinessContactsPage,
+    DisplayContactUpdatePage,
+    ProcessContactUpdatePage,
+    ProcessContactDelete
+    
 } from "../controllers/contact.controller.server.js";
 
 //instantiate router - on route
 const router = Router();
 
 router.get('/contact-login', DisplayLoginPage);
+router.get('/contact-login', ProcessLoginPage);
+router.get('/contact-list', DisplayBusinessContactsPage);
+router.get('/contact-list', ProcessBusinessContactsPage);
+router.get('/contact-update', DisplayContactUpdatePage);
+router.get('/contact-update', ProcessContactUpdatePage);
+router.get('/contact-update', ProcessContactDelete);
+
 
 export default router;
